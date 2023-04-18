39bd6ba0bb3da3f6e35abf11acc82c645130e48a
Sirbu-Boeti-Eduard <eduardboeti@gmail.com>
2023-04-18 21:28:27 +0300

    Log In & Register

--- app.js
+++ app.js
@@ -50,7 +50,95 @@ app.get("/login", (req, res) => {
     res.render("login");
 })
 
+app.get("/register", (req, res) => {
+    res.render("register");
+})
+
+
 //Starting the app
 app.listen(9000, ()=> {})
 
 require('./query.js');
+
+//Session
+const session = require('express-session');
+
+app.use(session({
+    secret: 'eduard',
+    resave: false,
+    saveUninitialized: true
+}));
+
+
+//Register
+const bcrypt = require("bcryptjs");
+//receive form values as json
+app.use(express.urlencoded({extended: 'false'}));
+app.use(express.json());
+
+app.post("/register", (req, res) => {    
+    const { username, passwd, passwd_cnf } = req.body;
+
+    db.query('SELECT username FROM user WHERE username = ?', [username], async (error, results) => {
+        if(error){
+            console.log(error);
+            res.render('register', {
+                message_register: 'An error occurred while checking username availability'
+            });
+        } else if( results.length > 0 ) {
+            res.render('register', {
+                message_register: 'This username is already in use'
+            });
+        } else if(passwd !== passwd_cnf) {
+            res.render('register', {
+                message_register: 'Passwords do not match!'
+            });
+        } else {
+            let hashedPassword = await bcrypt.hash(passwd, 8);
+
+            db.query('INSERT INTO user SET ?', {username: username, passwd: hashedPassword}, (error, result) => {
+                if(error) {
+                    console.log(error);
+                    res.render('register', {
+                        message_register: 'An error occurred while registering the user'
+                    });
+                } else {
+                    res.render('index', {
+                        message_register: 'User registered!'
+                    });
+                }
+            });
+        }
+     });
+});
+
+
+app.post("/login", (req, res) => {
+    const { username, passwd } = req.body;
+
+    db.query('SELECT * FROM user WHERE username = ?', [username], async (error, results) => {
+        if(error){
+            console.log(error);
+            res.render('login', {
+                message_login: 'An error occurred while fetching the user'
+            });
+        } else if( results.length == 0 ) {
+            res.render('login', {
+                message_login: 'Invalid credentials'
+            });
+        } else {
+            let isPasswordMatch = await bcrypt.compare(passwd, results[0].passwd);
+            if(isPasswordMatch){
+                // set user session and redirect to dashboard
+                req.session.user = results[0];
+                res.render('index', {
+                    message_login: 'Log In succesful'
+                })
+            } else {
+                res.render('login', {
+                    message_login: 'Invalid credentials'
+                });
+            }
+        }
+    });
+});
